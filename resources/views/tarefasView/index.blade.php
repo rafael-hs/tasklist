4e270515c8b7ca53641da3973840ff0e8e72da87
Rafael Honorio <rafael.contatotrab@gmail.com>
2018-08-18 16:53:50 -0300

    finalizado mas sem a identação

--- resources/views/tarefasView/index.blade.php
+++ resources/views/tarefasView/index.blade.php
@@ -2,10 +2,7 @@
 @section("content")
 <div>
        
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+        
             <div class="titulo">
                 <h1>Lista de Tarefas</h1>
             </div>
